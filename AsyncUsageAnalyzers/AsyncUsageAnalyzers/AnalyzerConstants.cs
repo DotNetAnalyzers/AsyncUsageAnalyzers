fee1652e72f9de8e51a8ba93c5dc876e94cd4a68
Sam Harwell <sam@tunnelvisionlabs.com>
2015-05-03 08:58:39 -0500

    Add AnalyzerConstants

---
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers/AnalyzerConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace AsyncUsageAnalyzers
+{
+    using Microsoft.CodeAnalysis;
+
+    internal static class AnalyzerConstants
+    {
+        static AnalyzerConstants()
+        {
+#if DEBUG
+            // In DEBUG builds, the tests are enabled to simplify development and testing.
+            DisabledNoTests = true;
+#else
+            DisabledNoTests = false;
+#endif
+        }
+
+        /// <summary>
+        /// Gets a reference value which can be passed to
+        /// <see cref="DiagnosticDescriptor(string, string, string, string, DiagnosticSeverity, bool, string, string, string[])"/>
+        /// to disable a diagnostic which is currently untested.
+        /// </summary>
+        /// <value>
+        /// A reference value which can be passed to
+        /// <see cref="DiagnosticDescriptor(string, string, string, string, DiagnosticSeverity, bool, string, string, string[])"/>
+        /// to disable a diagnostic which is currently untested.
+        /// </value>
+        internal static bool DisabledNoTests { get; }
+    }
+}
