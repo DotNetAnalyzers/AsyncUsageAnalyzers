cbb8b988b38c270a0fc60474f864caf95acbf323
Sam Harwell <sam@tunnelvisionlabs.com>
2015-12-30 20:50:43 -0600

    Implement diagnostic IncludeCancellationParameter

---
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers.Test/Usage/IncludeCancellationParameterUnitTests.cs
@@ -0,0 +1,310 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace AsyncUsageAnalyzers.Test.Usage
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AsyncUsageAnalyzers.Usage;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using TestHelper;
+    using Xunit;
+
+    public class IncludeCancellationParameterUnitTests : DiagnosticVerifier
+    {
+        public static IEnumerable<object[]> AsynchronousUnitTestWithReturnValue
+        {
+            get
+            {
+                yield return new[] { "    public          Task      MethodAsync(string param1, string param2) { return null; }", "TestMethod" };
+                yield return new[] { "    public          Task      MethodAsync(string param1, string param2) { return null; }", "Fact" };
+                yield return new[] { "    public          Task      MethodAsync(string param1, string param2) { return null; }", "Theory" };
+                yield return new[] { "    public          Task      MethodAsync(string param1, string param2) { return null; }", "Test" };
+                yield return new[] { "    public          Task<int> MethodAsync(string param1, string param2) { return null; }", "TestMethod" };
+                yield return new[] { "    public          Task<int> MethodAsync(string param1, string param2) { return null; }", "Fact" };
+                yield return new[] { "    public          Task<int> MethodAsync(string param1, string param2) { return null; }", "Theory" };
+                yield return new[] { "    public          Task<int> MethodAsync(string param1, string param2) { return null; }", "Test" };
+                yield return new[] { "    public          TASK      MethodAsync(string param1, string param2) { return null; }", "TestMethod" };
+                yield return new[] { "    public          TASK      MethodAsync(string param1, string param2) { return null; }", "Fact" };
+                yield return new[] { "    public          TASK      MethodAsync(string param1, string param2) { return null; }", "Theory" };
+                yield return new[] { "    public          TASK      MethodAsync(string param1, string param2) { return null; }", "Test" };
+            }
+        }
+
+        [Fact]
+        public async Task TestPropertyAsync()
+        {
+            string testCode = @"
+using System.Threading.Tasks;
+class ClassName
+{
+    Task PropertyName { get; set; }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestPrivateMemberAsync()
+        {
+            string testCode = @"
+using System.Threading.Tasks;
+class ClassName
+{
+    async Task Method1Async() { }
+    private async Task Method2Async() { }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestCancellationTokenInStructureAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task MethodAsync(Context context) { }
+}
+
+struct Context
+{
+    public CancellationToken CancellationToken { get; }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestContextStructureUsesMethodAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task MethodAsync(Context context) { }
+}
+
+struct Context
+{
+    public CancellationToken CancellationToken() => default(CancellationToken);
+}
+";
+
+            var expected = this.CSharpDiagnostic().WithArguments("MethodAsync").WithLocation(6, 23);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestContextStructurePropertyIsNotPublicAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task MethodAsync(Context context) { }
+}
+
+struct Context
+{
+    internal CancellationToken CancellationToken { get; }
+}
+";
+
+            var expected = this.CSharpDiagnostic().WithArguments("MethodAsync").WithLocation(6, 23);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestContextStructurePropertyHasWrongNameAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task MethodAsync(Context context) { }
+}
+
+struct Context
+{
+    public CancellationToken Token { get; }
+}
+";
+
+            var expected = this.CSharpDiagnostic().WithArguments("MethodAsync").WithLocation(6, 23);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestContextStructurePropertyReturnsAnotherTypeAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task MethodAsync(Context context) { }
+}
+
+struct Context
+{
+    public int CancellationToken { get; }
+}
+";
+
+            var expected = this.CSharpDiagnostic().WithArguments("MethodAsync").WithLocation(6, 23);
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestCancellationTokenInInterfaceAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task MethodAsync(IContext context) { }
+}
+
+interface IContext
+{
+    CancellationToken CancellationToken { get; }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestAsyncVoidMethodAsync()
+        {
+            string testCode = @"
+class ClassName
+{
+    public async void Method1Async() { }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestMethodsWithCancellationTokenAsync()
+        {
+            string testCode = @"
+using System.Threading;
+using System.Threading.Tasks;
+using CT = System.Threading.CancellationToken;
+class ClassName
+{
+    public async Task Method1Async(CancellationToken cancellationToken) { }
+    public async Task Method2Async(CT cancellationToken) { }
+    public async Task<int> Method3Async(CancellationToken cancellationToken) { return 0; }
+    public async Task<int> Method4Async(CT cancellationToken) { return 0; }
+    public Task Method5Async(CancellationToken cancellationToken) { return Task.FromResult(0); }
+    public Task Method6Async(CT cancellationToken) { return Task.FromResult(0); }
+    public Task<int> Method7Async(CancellationToken cancellationToken) { return Task.FromResult(0); }
+    public Task<int> Method8Async(CT cancellationToken) { return Task.FromResult(0); }
+}
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestMethodsWithoutCancellationTokenAsync()
+        {
+            string testCode = @"
+using System.Threading.Tasks;
+class ClassName
+{
+    public async Task Method1Async() { }
+    public async Task<int> Method2Async() { return 0; }
+    public Task Method3Async() { return Task.FromResult(0); }
+    public Task<int> Method4Async() { return Task.FromResult(0); }
+}
+";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithArguments("Method1Async").WithLocation(5, 23),
+                this.CSharpDiagnostic().WithArguments("Method2Async").WithLocation(6, 28),
+                this.CSharpDiagnostic().WithArguments("Method3Async").WithLocation(7, 17),
+                this.CSharpDiagnostic().WithArguments("Method4Async").WithLocation(8, 22),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestMethodsWithWrongCancellationTokenAsync()
+        {
+            string testCode = @"
+using System.Threading.Tasks;
+using CT = CancellationToken;
+class ClassName
+{
+    public async Task Method1Async(CancellationToken cancellationToken) { }
+    public async Task Method2Async(CT cancellationToken) { }
+    public async Task<int> Method3Async(CancellationToken cancellationToken) { return 0; }
+    public async Task<int> Method4Async(CT cancellationToken) { return 0; }
+    public Task Method5Async(CancellationToken cancellationToken) { return Task.FromResult(0); }
+    public Task Method6Async(CT cancellationToken) { return Task.FromResult(0); }
+    public Task<int> Method7Async(CancellationToken cancellationToken) { return Task.FromResult(0); }
+    public Task<int> Method8Async(CT cancellationToken) { return Task.FromResult(0); }
+}
+
+struct CancellationToken
+{
+}
+";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithArguments("Method1Async").WithLocation(6, 23),
+                this.CSharpDiagnostic().WithArguments("Method2Async").WithLocation(7, 23),
+                this.CSharpDiagnostic().WithArguments("Method3Async").WithLocation(8, 28),
+                this.CSharpDiagnostic().WithArguments("Method4Async").WithLocation(9, 28),
+                this.CSharpDiagnostic().WithArguments("Method5Async").WithLocation(10, 17),
+                this.CSharpDiagnostic().WithArguments("Method6Async").WithLocation(11, 17),
+                this.CSharpDiagnostic().WithArguments("Method7Async").WithLocation(12, 22),
+                this.CSharpDiagnostic().WithArguments("Method8Async").WithLocation(13, 22),
+            };
+
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [Theory]
+        [MemberData(nameof(AsynchronousUnitTestWithReturnValue))]
+        public async Task TestAsynchronousUnitTestMethodAsync(string declaration, string testAttribute)
+        {
+            var testCode = @"
+using System.Threading.Tasks;
+using TASK = System.Threading.Tasks.Task<int>;
+public class ClassName
+{
+    [##]
+$$
+}
+internal sealed class ##Attribute : System.Attribute { }
+";
+
+            await this.VerifyCSharpDiagnosticAsync(testCode.Replace("$$", declaration).Replace("##", testAttribute), EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+        {
+            yield return new IncludeCancellationParameterAnalyzer();
+        }
+    }
+}
---
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers/Usage/IncludeCancellationParameterAnalyzer.cs
@@ -0,0 +1,205 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace AsyncUsageAnalyzers.Usage
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Immutable;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// This analyzer reports a diagnostic if an asynchronous method does not include a <see cref="CancellationToken"/>
+    /// parameter.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class IncludeCancellationParameterAnalyzer : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The ID for diagnostics produced by the <see cref="IncludeCancellationParameterAnalyzer"/> analyzer.
+        /// </summary>
+        public const string DiagnosticId = "IncludeCancellationParameter";
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(UsageResources.IncludeCancellationParameterTitle), UsageResources.ResourceManager, typeof(UsageResources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(UsageResources.IncludeCancellationParameterMessageFormat), UsageResources.ResourceManager, typeof(UsageResources));
+        private static readonly string Category = "AsyncUsage.CSharp.Usage";
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(UsageResources.IncludeCancellationParameterDescription), UsageResources.ResourceManager, typeof(UsageResources));
+        private static readonly string HelpLink = "https://github.com/DotNetAnalyzers/AsyncUsageAnalyzers/blob/master/documentation/IncludeCancellationParameter.md";
+
+        private static readonly DiagnosticDescriptor Descriptor =
+            new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, AnalyzerConstants.EnabledByDefault, Description, HelpLink);
+
+        private static readonly Action<CompilationStartAnalysisContext> CompilationStartAction = HandleCompilationStart;
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(CompilationStartAction);
+        }
+
+        private static void HandleCompilationStart(CompilationStartAnalysisContext context)
+        {
+            Analyzer analyzer = new Analyzer(context.Compilation.GetOrCreateGeneratedDocumentCache(), context.Compilation);
+            context.RegisterSymbolAction(analyzer.HandleMethodDeclaration, SymbolKind.Method);
+        }
+
+        private sealed class Analyzer
+        {
+            private readonly ConcurrentDictionary<SyntaxTree, bool> generatedHeaderCache;
+            private INamedTypeSymbol cancellationTokenType;
+
+            public Analyzer(ConcurrentDictionary<SyntaxTree, bool> generatedHeaderCache, Compilation compilation)
+            {
+                this.generatedHeaderCache = generatedHeaderCache;
+                this.cancellationTokenType = compilation.GetTypeByMetadataName(typeof(CancellationToken).FullName);
+            }
+
+            public void HandleMethodDeclaration(SymbolAnalysisContext context)
+            {
+                IMethodSymbol symbol = (IMethodSymbol)context.Symbol;
+                if (this.cancellationTokenType == null)
+                {
+                    return;
+                }
+
+                if (symbol.DeclaredAccessibility == Accessibility.Private || symbol.DeclaredAccessibility == Accessibility.NotApplicable)
+                {
+                    return;
+                }
+
+                if (symbol.Locations.IsDefaultOrEmpty)
+                {
+                    return;
+                }
+
+                if (symbol.IsOverride)
+                {
+                    return;
+                }
+
+                if (!symbol.ExplicitInterfaceImplementations.IsDefaultOrEmpty)
+                {
+                    return;
+                }
+
+                Location location = symbol.Locations[0];
+                if (!location.IsInSource || location.SourceTree.IsGeneratedDocument(this.generatedHeaderCache, context.CancellationToken))
+                {
+                    return;
+                }
+
+                // void-returning methods are not asynchronous according to their signature, even if they use `async`
+                if (symbol.ReturnsVoid)
+                {
+                    return;
+                }
+
+                if (!symbol.IsAsync)
+                {
+                    // This check conveniently covers Task and Task<T> by ignoring the `1 in Task<T>.
+                    if (!string.Equals(nameof(Task), symbol.ReturnType?.Name, StringComparison.Ordinal))
+                    {
+                        return;
+                    }
+
+                    if (!string.Equals(typeof(Task).Namespace, symbol.ReturnType?.ContainingNamespace?.ToString(), StringComparison.Ordinal))
+                    {
+                        return;
+                    }
+                }
+
+                if (symbol.MethodKind == MethodKind.PropertyGet || symbol.MethodKind == MethodKind.PropertySet)
+                {
+                    return;
+                }
+
+                if (IsTestMethod(symbol))
+                {
+                    return;
+                }
+
+                foreach (var parameterSymbol in symbol.Parameters)
+                {
+                    INamedTypeSymbol parameterType = parameterSymbol.Type as INamedTypeSymbol;
+                    if (this.cancellationTokenType.Equals(parameterType))
+                    {
+                        return;
+                    }
+                }
+
+                if ((symbol.ContainingType.TypeKind == TypeKind.Class || symbol.ContainingType.TypeKind == TypeKind.Struct)
+                    && symbol.DeclaredAccessibility == Accessibility.Public)
+                {
+                    // As a final check, make sure the method isn't implicitly implementing an interface method
+                    foreach (INamedTypeSymbol interfaceType in symbol.ContainingType.AllInterfaces)
+                    {
+                        foreach (var member in interfaceType.GetMembers(symbol.Name))
+                        {
+                            if (Equals(symbol.ContainingType.FindImplementationForInterfaceMember(member), symbol))
+                            {
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                foreach (var parameterSymbol in symbol.Parameters)
+                {
+                    if (parameterSymbol.RefKind == RefKind.Out)
+                    {
+                        continue;
+                    }
+
+                    foreach (var member in parameterSymbol.Type.GetMembers(nameof(CancellationToken)))
+                    {
+                        if (member.Kind != SymbolKind.Property)
+                        {
+                            continue;
+                        }
+
+                        if (member.DeclaredAccessibility != Accessibility.Public)
+                        {
+                            continue;
+                        }
+
+                        var propertySymbol = (IPropertySymbol)member;
+                        if (this.cancellationTokenType.Equals(propertySymbol.Type))
+                        {
+                            return;
+                        }
+                    }
+                }
+
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, symbol.Locations[0], symbol.Name));
+            }
+
+            private static bool IsTestMethod(IMethodSymbol methodSymbol)
+            {
+                foreach (AttributeData attributeData in methodSymbol.GetAttributes())
+                {
+                    var attributeClass = attributeData.AttributeClass;
+                    if (attributeClass == null)
+                    {
+                        continue;
+                    }
+
+                    if (string.Equals(attributeClass.Name, "TestMethodAttribute", StringComparison.Ordinal)
+                        || string.Equals(attributeClass.Name, "FactAttribute", StringComparison.Ordinal)
+                        || string.Equals(attributeClass.Name, "TheoryAttribute", StringComparison.Ordinal)
+                        || string.Equals(attributeClass.Name, "TestAttribute", StringComparison.Ordinal))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
